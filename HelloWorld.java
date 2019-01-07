afb94c085bcf8afb2ee51a86b9bf4ec3c6c4f998
Abishek <apotlapalli@drtstrategies.com>
2019-01-07 00:46:15 -0500

    DOne

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World. My First Java Program after loooooong time");
-		System.out.println("One Step more...");
 	}
 }

b925962b3a91e86ee872fc40a7dc9ce0cd1c6b1e
Abishek <apotlapalli@drtstrategies.com>
2019-01-08 15:02:49 -0500

    Pavan

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World. My First Java Program after loooooong time");
-		System.out.println("Finally, fixed the code to see Console Output");
+		System.out.println("Hello Pavan...You rock.,");
 
 	}
 }
